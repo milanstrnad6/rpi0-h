63d6376b38703b98d897f52c3d5f286aa7bf499c
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-11-16 02:27:37 +0100

    Fiiix argh

--- serverStart.sh
+++ serverStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo fuser 80/tcp -k
-export FLASK_APP=/home/pi/Desktop/rpi0-h/API.py
+export FLASK_APP=/home/pi/Desktop/FINAL/rpi0-h/API.py
 sudo -E flask run --host=0.0.0.0 --port=80
